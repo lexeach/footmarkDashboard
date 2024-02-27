a2fbf135a32541e939685604f931c0f0f1b0d426
lexeach <80155340+lexeach@users.noreply.github.com>
2024-02-27 18:51:17 +0530

    Update home.js

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -953,27 +953,7 @@ const Dashboard = () => {
             </div>
           </div>
 
-          {/* Pool Income  */}
-          <div className="col-lg-4 col-md-6 col-sm-12 grid-margin">
-            <div className="card">
-              <div className="card-body">
-                <h5>Pool Income</h5>
-                <h4 className="mb-0">{pool3Income ? pool3Income : 0}</h4>
-              </div>
-            </div>
-          </div>
-          {/* Income USed  */}
-          <div className="col-lg-4 col-md-6 col-sm-12 grid-margin">
-            <div className="card">
-              <div className="card-body">
-                <h5> Income Used</h5>
-                <h4 className="mb-0">
-                  {pool3UsedIncome ? pool3UsedIncome : 0}
-                </h4>
-              </div>
-            </div>
-          </div>
-
+         
           {/* Income Balance  */}
           <div className="col-lg-4 col-md-6 col-sm-12 grid-margin">
             <div className="card">
